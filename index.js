989959793b7e3559a7847f4d500b6116bcbe1716
nanukashubladze <123475291+nanukashubladze@users.noreply.github.com>
2023-06-14 11:39:57 +0400

    all done

---
+++ index.js
@@ -0,0 +1,42 @@
+let month = "september";
+ switch(month){
+    case "jenuary":
+        console.log("winter")
+        break;
+        case "february":
+        console.log("winter")
+        break;
+        case "march":
+        console.log("spring")
+        break;
+        case "april":
+        console.log("spring")
+        break;
+        case "may":
+        console.log("spring")
+        break;
+        case "june":
+        console.log("summer")
+        break;
+        case "july":
+        console.log("summer")
+        break;
+        case "august":
+        console.log("summer")
+        break;
+        case "september":
+        console.log("autumn")
+        break;
+        case "octomber":
+        console.log("autumn")
+        break;
+        case "november":
+        console.log("autumn")
+        break;
+        case "december":
+        console.log("winter")
+        break;
+        default:
+        console.log("this is not month")
+        break;
+ }
